694090b586b309436baecf972a08726db0d66d18
Anthony Cano <132002947+AnthonyCano@users.noreply.github.com>
2025-04-14 21:08:34 -0700

    adding the variables I will be using and manipulating

--- Haunted-Jaunt-main/Assets/UnityTechnologies/3DBeginnerTutorialComplete/Scripts/EnemyGlowEffect.cs
+++ Haunted-Jaunt-main/Assets/UnityTechnologies/3DBeginnerTutorialComplete/Scripts/EnemyGlowEffect.cs
@@ -1,7 +1,19 @@
 using UnityEngine;
 
+/* 
+My thoughts are just using the tag system and if john lemon gets within a certain distance
+to the enemy they start to glow. Then if I really wanted to I can add the particle trigger
+and sound when he gets even closer?
+*/ 
 public class EnemyGlowEffect : MonoBehaviour
 {
-
+    private Transform player;
+    private Renderer enemyRenderer;
+    
+    private float minDistance = 3f;
+    private float maxDistance = 8f;
+    
+    public Color farColor =  Color.white;
+    public Color closeColor = Color.red;
     
 }
